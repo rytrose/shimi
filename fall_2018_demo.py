f646f7842a56c0d7d7e93b591b5753d7a28e4149
Ryan Rose <rytrose@gmail.com>
2019-01-08 16:59:45 -0500

    add saving of input audio

--- fall_2018_demo.py
+++ fall_2018_demo.py
@@ -66,7 +66,7 @@ def main():
 
         # Set up wakeword
         wakeword = WakeWord(shimi=shimi, model="wakeword/resources/models/Hey-Shimi2.pmdl", on_wake=Alert,
-                            on_phrase=dialogue, respeaker=True, use_doa=True)#, manual_wake=True)
+                            on_phrase=dialogue, respeaker=True, use_doa=True, manual_wake=True)
 
         wakeword.start()
 
