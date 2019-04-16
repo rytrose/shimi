0f4bd13da0952d44c4cb2576e37f7ebcc98a509b
Ryan Rose <rytrose@gmail.com>
2019-04-16 17:40:26 -0400

    added ability for Shimi to sing with phonemes, still WIP and waiting on better samples

--- audio/singing.py
+++ audio/singing.py
@@ -18,17 +18,22 @@ import time
 import random
 import glob
 import argparse
+import pickle
+import markovify
+import json
 
 
 class PVSample:
     """Used to play an audio file with real-time pitch shifting and time stretching."""
 
-    def __init__(self, path, balance=None):
+    def __init__(self, path, balance=None, attack=0.005, release=0.05):
         """Initializes pyo objects.
 
         Args:
             path (str): A path to the audio file to play.
             balance (pyo.Balance, optional): A pyo object to balance the RMS of this sample player with.
+            attack (float): The time in seconds of the envelope attack when play() is called.
+            release (float): The time in seconds of the envelope release when stop() is called.
         """
         PVSIZE = 1024
         PVOLAPS = 4
@@ -45,7 +50,7 @@ class PVSample:
         self.trans_value = SigTo(1, time=0.005)
         self.pv_buffer = PVBuffer(
             self.pv_analysis, self.pointer, pitch=self.trans_value, length=self.LENGTH)
-        self.adsr = Adsr(attack=0.005, release=0.05)
+        self.adsr = Adsr(attack=attack, release=release)
 
         if balance is None:
             self.pv_synth = PVSynth(self.pv_buffer, mul=self.adsr)
@@ -54,21 +59,21 @@ class PVSample:
             self.pv_synth = PVSynth(self.pv_buffer)
             self.output = Balance(self.pv_synth, balance, mul=self.adsr)
 
+        self.output.out()
+
     def play(self):
         """Starts playback via the envelope generator and adds the playback to the audio server graph."""
         self.adsr.play()
-        self.output.out()
 
     def stop(self):
         """Stops playback via the envelope generator and removes the playback object from the audio server graph."""
         self.adsr.stop()
-        CallAfter(lambda: self.output.stop(), self.adsr.release)
 
     def set_transposition(self, val):
         """Changes the transposition of playback.
 
         Args:
-            val: A value in semitones of transposition from normal playback.
+            val: The factor of transposition, where 1 is original pitch, 2 is an octave higher, 0.5 is an octave lower, etc.
         """
         self.trans_value.setValue(val)
 
@@ -86,11 +91,20 @@ class PVSample:
         Args:
             val (float): A location between 0.0 and 1.0 where 0.0 is the beginning of the sample, and 1.0 is the end.
         """
+        self.pointer.reset()
         self.pointer.setPhase(val)
+
+    def set_balance(self, val):
+        """Sets a pyo.Balance object to balance sample output with.
+
+        Args:
+            val (pyo.Balance): An audio signal to balance energy output of this sample with.
+        """
+        self.output = Balance(self.pv_synth, val, mul=self.adsr)
 
 
 class Singing:
-    def __init__(self, init_pyo=False, duplex=False, resource_path="/home/nvidia/shimi/audio"):
+    def __init__(self, init_pyo=False, duplex=False, resource_path="/home/nvidia/shimi/audio", phoneme_style=False):
         """Initializes audio server if needed and establishes resource path.
 
         Args:
@@ -99,29 +113,47 @@ class Singing:
             resource_path (str, optional): The path to directory containing singing files.
         """
         self.duplex = duplex
+        self.phoneme_style = phoneme_style
         self.vocal_paths = glob.glob(
             op.join(resource_path, "audio_files", "shimi_vocalizations", "*"))
-        self.shimi_vocal_path = op.join(
-            resource_path, "audio_files", "shimi_vocalization.wav")
+        if self.phoneme_style:
+            self.shimi_vocal_path = op.join(
+                resource_path, "audio_files", "shimi_sounds", "shimi_phonemes_stereo.wav")
+        else:
+            self.shimi_vocal_path = op.join(
+                resource_path, "audio_files", "shimi_vocalization.wav")
         self.resource_path = resource_path
         self.playing = False
+        
 
         if init_pyo:
             self.server = Server()
             pa_list_devices()
 
             # Local testing
-            # self.server = Server()
-            if self.duplex:
-                self.server = Server(sr=16000, ichnls=4)
-                self.server.setInOutDevice(2)
-            else:
-                self.server = Server(sr=16000, duplex=0)
-                self.server.setOutputDevice(2)
+            self.server = Server()
+            # if self.duplex:
+            #     self.server = Server(sr=16000, ichnls=4)
+            #     self.server.setInOutDevice(2)
+            # else:
+            #     self.server = Server(sr=16000, duplex=0)
+            #     self.server.setOutputDevice(2)
             self.server.deactivateMidi()
             self.server.boot().start()
 
-    def audio_initialize(self, audio_path, extraction_type="cnn", extraction_file=None):
+        """
+        N.B. Phase Vocoder analysis is very CPU intensive, so to add various Shimi utterances is is necessary to
+            combine them into a single audio file, that is indexed into at the desired utterance for playback. This
+            necessitates the self.num_utterances variable which defines where to index into the Shimi file.
+        """
+        self.shimi_sample = PVSample(self.shimi_vocal_path)
+
+        self.phoneme_model_json = open('phoneme_markov_model.json', 'r').read()
+        self.phoneme_model = markovify.Text.from_json(self.phoneme_model_json)
+        self.phoneme_map = pickle.load(open(op.join(
+            self.resource_path, "audio_files", "shimi_sounds", "phoneme_map.p"), 'rb'))
+
+    def audio_load_and_extraction(self, audio_path, extraction_type="cnn", extraction_file=None):
         """Loads audio files, and loads or runs melody extraction.
 
         Args:
@@ -134,7 +166,7 @@ class Singing:
             self.path)
         self.song_length_in_samples = int(self.song_length_in_samples)
         self.song_sr = int(self.song_sr)
-        self.song_vol = 0.1
+        self.song_vol = 0.02
         self.song_sample = SfPlayer(self.path, mul=self.song_vol)
         self.vocal_filter = Biquadx(
             self.song_sample * (1 / self.song_vol), freq=800, q=0.75, type=2)
@@ -166,37 +198,27 @@ class Singing:
                 self.melody_timestamps = self.melody_extraction.deep_learning_timestamps
 
         self.length_seconds = self.melody_extraction.length_seconds
-
-        """
-        N.B. Phase Vocoder analysis is very CPU intensive, so to add various Shimi utterances is is necessary to
-            combine them into a single audio file, that is indexed into at the desired utterance for playback. This
-            necessitates the self.num_utterances variable which defines where to index into the Shimi file.
-        """
-        self.shimi_sample = PVSample(
-            self.shimi_vocal_path, balance=self.vocal_filter)
-        self.num_utterances = 4
-
         self.frequency_timestep = self.melody_timestamps[1] - \
             self.melody_timestamps[0]
+        self.shimi_sample.set_balance(self.vocal_filter)
+
+    def random_utterance_setup(self):
+        self.num_utterances = 4
         self.speed_index = 0
         self.speeds = []
+        self.playback_start_indices_index = 0
+        self.playback_start_indices = []
 
         for note in self.melody_extraction.notes:
-            speed = 1 / (self.num_utterances * (((note['end'] - note['start']) * self.frequency_timestep) + (
-                2 * self.shimi_sample.adsr.release)))
+            speed = 1 / (self.num_utterances * (((note['end'] - note['start'])
+                                                 * self.frequency_timestep) + (2 * self.shimi_sample.adsr.release)))
             self.speeds.append(float(speed))
+            self.playback_start_indices.append(
+                (1 / self.num_utterances) * random.randint(0, 3))
 
-        # The function self.set_freq is called every self.frequency_timestep seconds as a result of this Pattern object
-        self.frequency_setter = Pattern(
-            self.set_freq, time=float(self.frequency_timestep))
-        self.frequency_index = 1
-        self.prev_freq = self.melody_data[0]
+        print(self.playback_start_indices)
 
-        print("Waiting for PV Analysis to be done...")
-        wait_time = 3
-        for i in range(wait_time):
-            print("%d..." % (wait_time - i))
-            time.sleep(1.0)
+        self.define_singing_pattern()
 
     def set_freq(self):
         """Updates Shimi's voice playback to the appropriate frequency per melody extraction."""
@@ -205,9 +227,10 @@ class Singing:
         self.shimi_sample.set_transposition(new_transposition)
 
         if self.prev_freq <= 0 and new_freq > 0:
-            self.start_vocal()
+            self.start_note(
+                self.speeds[self.speed_index], self.playback_start_indices[self.playback_start_indices_index])
         elif self.prev_freq > 0 and new_freq <= 0:
-            self.end_vocal()
+            self.end_note()
 
         self.prev_freq = new_freq
 
@@ -221,15 +244,16 @@ class Singing:
         self.frequency_index = (self.frequency_index +
                                 1) % len(self.melody_data)
 
-    def start_vocal(self):
+    def start_note(self, speed, phase):
         """Sets the time-stretch factor at the beginning of every Shimi note."""
-        self.shimi_sample.set_speed(self.speeds[self.speed_index])
-        self.shimi_sample.set_phase(
-            (1 / self.num_utterances) * random.randint(0, 3))
+        self.shimi_sample.set_speed(speed)
+        self.shimi_sample.set_phase(phase)
         self.shimi_sample.play()
         self.speed_index = (self.speed_index + 1) % len(self.speeds)
+        self.playback_start_indices_index = (
+            self.playback_start_indices_index + 1) % len(self.playback_start_indices)
 
-    def end_vocal(self):
+    def end_note(self):
         """Stops Shimi playback at the end of every Shimi note."""
         self.shimi_sample.stop()
 
@@ -243,8 +267,12 @@ class Singing:
             starting_callback (function, optional): Function to call when singing playback starts. Can be used to start getsure.
             blocking (bool, optional): Determines whether or not this function should wait for playback to finish to return.
         """
-        self.audio_initialize(
+        self.audio_load_and_extraction(
             audio_path, extraction_type=extraction_type, extraction_file=extraction_file)
+        if self.phoneme_style:
+            self.phoneme_setup()
+        else:
+            self.random_utterance_setup()
         self.frequency_setter.play()
         self.delay = 0.01
         self.song_sample.out(delay=self.delay)
@@ -257,6 +285,91 @@ class Singing:
         if blocking:
             while self.playing:
                 pass
+
+    def define_singing_pattern(self):
+        # The function self.set_freq is called every self.frequency_timestep seconds as a result of this Pattern object
+        self.frequency_setter = Pattern(
+            self.set_freq, time=float(self.frequency_timestep))
+        self.frequency_index = 1
+        self.prev_freq = self.melody_data[0]
+
+    def phoneme_setup(self):
+        self.speed_index = 0
+        self.speeds = []
+        self.playback_start_indices_index = 0
+        self.playback_start_indices = []
+        self.syllables = []
+
+        for note in self.melody_extraction.notes:
+            note_length = (note['end'] - note['start']) * \
+                self.frequency_timestep
+            if not self.syllables:
+                new_word_list = self.phoneme_model.make_sentence().split(' ')
+                new_syllable = []
+                for phoneme in new_word_list:
+                    if "0" in phoneme or "1" in phoneme or "2" in phoneme:
+                        if not new_syllable:
+                            new_syllable.append(phoneme[:-1])
+                        else:
+                            self.syllables.append(new_syllable)
+                            new_syllable = [phoneme[:-1]]
+                    else:
+                        new_syllable.append(phoneme)
+                if new_syllable:
+                    self.syllables.append(new_syllable)
+
+            current_syllable = self.syllables.pop(0)
+            syllable_length = 0
+            for phoneme in current_syllable:
+                phoneme_info = self.phoneme_map[phoneme + '.wav']
+                self.playback_start_indices.append(phoneme_info['norm_start'])
+                syllable_length += phoneme_info['length']
+
+            for phoneme in current_syllable:
+                playback_ratio = 1 / phoneme_info['rel_length']
+                speed = 1 / (playback_ratio * ((phoneme_info['length'] / syllable_length) + (
+                    2 * self.shimi_sample.adsr.release)))
+                self.speeds.append(speed)
+
+        self.define_singing_pattern()
+
+    def say_word(self):
+        word = self.phoneme_model.make_sentence()
+        print(word)
+        word_list = word.split(' ')
+        
+        for i, phoneme in enumerate(word_list):
+            if "0" in phoneme or "1" in phoneme or "2" in phoneme:
+                word_list[i] = phoneme[:-1]
+
+        def play_syllable(arg):
+            print("Playing: starting at %.4f, expected %.4f" % (time.time() - arg[2], arg[3]))
+            arg[0].shimi_sample.set_phase(arg[1])
+
+        def stop_syllable(arg):
+            print("Stopping: %.4f time elapsed, expected %.4f." % (time.time() - arg[1], arg[2]))
+            arg[0].shimi_sample.stop()
+
+        self.calls = []
+        self.shimi_sample.play()
+        t = time.time()
+        delay = 0
+        total_time = 0
+        for i, phoneme in enumerate(word_list):
+            phoneme_info = self.phoneme_map[phoneme + '.wav']
+            total_time += phoneme_info['length']
+            self.calls.append(CallAfter(play_syllable, delay, (self, phoneme_info['norm_start'], t, delay)))
+            delay += phoneme_info['length']
+
+            if i == len(word_list) - 1:
+                self.calls.append(CallAfter(stop_syllable, delay, (self, t, total_time)))
+
+    def say_phoneme(self, phoneme):
+        phoneme_info = self.phoneme_map[phoneme + '.wav']
+        pos = phoneme_info['norm_start']
+        self.shimi_sample.set_phase(pos)
+        self.shimi_sample.play()
+        self.call = CallAfter(lambda: self.shimi_sample.stop(), phoneme_info['length'])
 
     def sing_midi(self, midi_path):
         """Sings a specified MIDI file.
@@ -311,4 +424,4 @@ if __name__ == '__main__':
     parser.add_argument("-p", "--pyo", action="store_true", default=False)
     parser.add_argument("-r", "--resource_path", type=str, default=os.getcwd())
     args = parser.parse_args()
-    s = Singing(init_pyo=args.pyo, resource_path=args.resource_path)
+    s = Singing(init_pyo=args.pyo, resource_path=args.resource_path, phoneme_style=False)
